13dc7684f85725962856468336c20f76232eb6c2
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2021-06-16 16:03:59 +0600

    nodemon

--- 03. NPM/index.js
+++
@@ -1,6 +0,0 @@
-const chalk = require('chalk')
-
-console.log(chalk.red.bold('I am Edited Text'))
-
-console.log('I Am Installed nodemon')
-console.log('This is Another New Line')
---
+++ 03.NPM/index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk')
+
+console.log(chalk.red.bold('Check Installed Chalk'))
+console.log(chalk.white.inverse('I am Edited Text'))
+console.log('Another Text')
